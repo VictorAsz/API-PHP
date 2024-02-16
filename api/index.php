9f5155c43d3b84618366ab3c8a3eaaaf4641aa2c
Victor Assunção <victor.a.schumann@gmail.com>
2024-02-16 17:20:35 -0300

    first commit

---
+++ api/index.php
@@ -0,0 +1,28 @@
+<?php 
+
+$data = [];
+
+if(isset($_GET['options'])){
+
+    switch($_GET['option']) {
+        case 'status':
+            $data['status'] = 'SUCCESS';
+            $data['data'] = 'API running OK!';
+        break;
+        
+        default:
+            $data['status'] = 'ERROR';
+        break;
+    }
+} else {
+    $data['status'] = 'ERROR';
+}
+
+
+function response($data_response)
+{
+    header('Content-Type:application/json');
+    echo json_encode($data_response);  
+}
+
+?>
