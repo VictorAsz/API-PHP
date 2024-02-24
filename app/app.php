898217c383fedf11bebf2cae0ad401b3191b65ff
Victor Assunção <victor.a.schumann@gmail.com>
2024-02-24 18:25:59 -0300

    api request complete

--- app/app.php
+++ app/app.php
@@ -2,9 +2,16 @@
 
 define('API_BASE', 'http://localhost/API%20PHP/api/?option=' );
 
+
+echo '<h3>APLICAÇÃO<h3/><hr>';
+
+$resultado = api_request('status');
+echo $resultado;
 function api_request($option){
  $client = curl_init(API_BASE . $option);
  curl_setopt($client, CURLOPT_RETURNTRANSFER, true);
+ $response = curl_exec($client);
+ return $response;
 }
 
 ?>
