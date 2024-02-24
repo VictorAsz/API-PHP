b4e119ad0ba7bea8864246b10e7fac602019562b
Victor Assunção <victor.a.schumann@gmail.com>
2024-02-24 18:22:20 -0300

    curl_setopt

--- app/app.php
+++ app/app.php
@@ -4,6 +4,7 @@ define('API_BASE', 'http://localhost/API%20PHP/api/?option=' );
 
 function api_request($option){
  $client = curl_init(API_BASE . $option);
+ curl_setopt($client, CURLOPT_RETURNTRANSFER, true);
 }
 
 ?>
