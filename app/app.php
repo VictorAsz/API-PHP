780849bb51868e477a200c7433c6c0f4b23a26a8
Victor Assunção <victor.a.schumann@gmail.com>
2024-02-25 18:33:06 -0300

    formatação do json

--- app/app.php
+++ app/app.php
@@ -6,12 +6,13 @@ define('API_BASE', 'http://localhost/API%20PHP/api/?option=' );
 echo '<h3>APLICAÇÃO<h3/><hr>';
 
 $resultado = api_request('status');
-echo $resultado;
+echo '<pre>';
+print_r($resultado);
 function api_request($option){
  $client = curl_init(API_BASE . $option);
  curl_setopt($client, CURLOPT_RETURNTRANSFER, true);
  $response = curl_exec($client);
- return $response;
+ return json_decode($response, true);
 }
 
 ?>
